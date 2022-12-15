f36938ad3ae827e216de9870aa050a7a8df111a4
MohamadGhouseJaved <mohamedghousejaved@gmail.com>
2022-12-15 20:47:21 +0530

    Initial commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ a = int(input("Enter a decimal number: "))
 b = 0
 c = 1
 while a > 0:
-        b = b +(a % 2)* c
-        a =a// 2
+        b= b +(a % 2)* c
+        a=a// 2
         c =c*10
 print(b)
